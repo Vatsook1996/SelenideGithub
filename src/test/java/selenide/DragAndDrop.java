82cf0414fdef9c9470caccf68f374c3876be2273
Vatsook1996 <ahmetdianovv@mail.ru>
2023-04-22 12:37:55 +0300

    Init commit2

--- src/test/java/selenide/DragAndDrop.java
+++ src/test/java/selenide/DragAndDrop.java
@@ -1,14 +1,13 @@
 package selenide;
 
 import org.junit.jupiter.api.Test;
-
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
 
-public class dragAndDrop {
+public class DragAndDrop {
     @Test
-    void dragAndDrop() {
+    void dragAndDropTest() {
         // Открыть страницу https://the-internet.herokuapp.com/drag_and_drop
         open ("https://the-internet.herokuapp.com/drag_and_drop");
         // Переместить прямоугольник А на место В
@@ -16,5 +15,7 @@ public class dragAndDrop {
         // Проверка, что прямоугольники действительно поменялись
         $("#column-a").shouldHave(text("B"));
         $("#column-b").shouldHave(text("A"));
+
+
 }
 }
